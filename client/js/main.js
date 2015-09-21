10e9b11300232f4ebdfad4f7ceb0ae3fa72a5ef3
Natalie Wolfe <natalie@lifewanted.com>
2015-09-20 18:32:54 -0700

    Trying phaser instead

--- client/js/main.js
+++ client/js/main.js
@@ -1,22 +1,52 @@
 "use strict";
 
-var Renderer    = require('./lib/Renderer');
-var Game        = require('./lib/Game');
-var Kerbal      = require('./game/Kerbal');
-
 window.onload = function(){
-    var renderer = new Renderer();
-    var game = new Game(renderer);
+    var platforms = null;
+    var kerb = null;
+    var game = new Phaser.Game(800, 600, Phaser.AUTO, 'viewer', {
+        preload: function(){},
+        create: function(){
+            kerb = game.add.graphics(0, 0);
+            kerb
+                .beginFill(0xbada55)
+                .drawCircle(10, 10, 10)
+                .endFill()
+            ;
+        },
+        update: function(){
+            if (keys.w) {
+                kerb.position.y -= 1;
+            }
+            if (keys.s) {
+                kerb.position.y += 1;
+            }
+            if (keys.a) {
+                kerb.position.x -= 1;
+            }
+            if (keys.d) {
+                kerb.position.x += 1;
+            }
+        }
+    });
+}
 
-    game.add(new Kerbal(renderer.root));
-
-    var prevStart = Date.now();
-    function animate(){
-        requestAnimationFrame(animate);
-        var thisStart = Date.now();
-        game.step(thisStart - prevStart);
-        renderer.render();
-        prevStart = thisStart;
-    }
-    requestAnimationFrame(animate);
-};
+// var Renderer    = require('./lib/Renderer');
+// var Game        = require('./lib/Game');
+// var Kerbal      = require('./game/Kerbal');
+//
+// window.onload = function(){
+//     var renderer = new Renderer();
+//     var game = new Game(renderer);
+//
+//     game.add(new Kerbal(renderer.root));
+//
+//     var prevStart = Date.now();
+//     function animate(){
+//         requestAnimationFrame(animate);
+//         var thisStart = Date.now();
+//         game.step(thisStart - prevStart);
+//         renderer.render();
+//         prevStart = thisStart;
+//     }
+//     requestAnimationFrame(animate);
+// };
